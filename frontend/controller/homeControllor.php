2d1d925f54590e5fbc64c6759f4c83b3f8f31365
DESKTOP-7LL627M\huuqu <huuquynh142@gmail.com>
2017-11-05 01:30:21 +0700

    get_data_home

--- frontend/controller/homeControllor.php
+++ frontend/controller/homeControllor.php
@@ -9,19 +9,44 @@
  include('../../../key/ProductKey.php');
  class HomeControllor extends database{
      function index(){
-         $sql = 'select ' .ProductKey::PRODUCT_ID_DB.' , '
-                          .ProductKey::NAME_DB.' , '
-                          .ProductKey::SALE_PRICE_DB .' , '
-                          .ProductKey::IMAGE_DB.' from  '
-                          .ProductKey::PRODUCT_TABLE.' , '
-                          .ProductKey::PRODUCT_DETAIL_TABLE.' where '
-                          .ProductKey::PRODUCT_DETAIL_ID_DB.' = '
-                          .ProductKey::DETAIL_ID_DB.' limit 10';
+         $sql = 'select '
+              .ProductKey::PRODUCT_ID_DB.' , '
+              .ProductKey::NAME_DB.' , '
+              .ProductKey::SALE_PRICE_DB .' , '
+              .ProductKey::IMAGE_DB.' from  '
+              .ProductKey::PRODUCT_TABLE.' , '
+              .ProductKey::PRODUCT_DETAIL_TABLE.' where '
+              .ProductKey::PRODUCT_DETAIL_ID_DB.' = '
+              .ProductKey::DETAIL_ID_DB.' limit 10';
          return array('body'=> $this->getAllData($sql));
      }
 
      function slide(){
-        $sql = "select * from slide order by created_at DESC limit 5";
+         $sql = 'select '
+             .ProductKey::PRODUCT_ID_DB.' , '
+             .ProductKey::NAME_DB.' , '
+             .ProductKey::SALE_PRICE_DB .' , '
+             .ProductKey::COMMENT_DB.' , '
+             .ProductKey::IMAGE_DB.' from  '
+             .ProductKey::PRODUCT_TABLE.' , '
+             .ProductKey::PRODUCT_DETAIL_TABLE.' where '
+             .ProductKey::PRODUCT_DETAIL_ID_DB.' = '
+             .ProductKey::DETAIL_ID_DB.' and '
+             .ProductKey::IS_SLIDE_DB.' = 1';
          return array('body'=>$this->getAllData($sql));
      }
+     function carousel(){
+         $sql = 'select '
+             .ProductKey::PRODUCT_ID_DB.' , '
+             .ProductKey::NAME_DB.' , '
+             .ProductKey::SALE_PRICE_DB .' , '
+             .ProductKey::IMAGE_DB.' from  '
+             .ProductKey::PRODUCT_TABLE.' , '
+             .ProductKey::PRODUCT_DETAIL_TABLE.' where '
+             .ProductKey::PRODUCT_DETAIL_ID_DB.' = '
+             .ProductKey::DETAIL_ID_DB.' order by '
+             .ProductKey::CREATED_AT_DB.' DESC limit 8';
+         return array('body'=>$this->getAllData($sql));
+     }
+
  }
--- frontend/public/view/slide.php
+++ frontend/public/view/slide.php
@@ -1,15 +1,10 @@
-<?php
-include_once("../../controller/homeControllor.php");
-$c = new HomeControllor();
-$h =$c->index();
-?>
 <div id="da-slider" class="da-slider">
-    <?php foreach($h['body'] as $item ){ ?>
+    <?php foreach($slide['body'] as $item ){ ?>
     <div class="da-slide">
-        <h2>welcome to aditii</h2>
-        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
+        <h2><?=$item[ProductKey::NAME]?> </h2>
+        <p><?=$item[ProductKey::COMMENT] ?></p>
         <a href="details.html" class="da-link">shop now</a>
-        <div class="da-img"><img src="../lib/images/slider1.png" alt="image01" /></div>
+        <div class="da-img"><img src="../lib/images/<?= $item[ProductKey::IMAGE] ?>" alt="image01" /></div>
     </div>
     <?php }?>
     <nav class="da-arrows">
